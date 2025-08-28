c0b82f79a282d18ef90d73588a0449b5159b60a0
yuvrajN0512 <yuvrajn0512@gmail.com>
2025-08-28 21:04:55 +0530

    footer section
    footer section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HerosectionComponent } from './herosection/herosection.component';
+import { ServicesSectionComponent } from './services-section/services-section.component';
+import { ContactsectionComponent } from './contactsection/contactsection.component';
+import { JourneysectionComponent } from './journeysection/journeysection.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/hero', pathMatch: 'full' } ,// default route
   { path: 'hero', component: HerosectionComponent },
+  { path: 'service', component: ServicesSectionComponent},
+  { path: 'contact', component: ContactsectionComponent },
+  { path: 'about', component: JourneysectionComponent },
 ];
 
 @NgModule({
